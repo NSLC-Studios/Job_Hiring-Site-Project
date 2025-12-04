2141300e399c37b181ef75073918a7d8133445cb
Nick <kiss.miklos20k@egressy.info>
2025-12-04 12:28:02 +0100

    Added Persistence

---
+++ API/JobHiringAPI/JobHiringAPI/Persistence/JobDatabaseContext.cs
@@ -0,0 +1,165 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.PortableExecutable;
+
+namespace JobHiringAPI.Persistence
+{
+    public class JobDatabaseContext // ADD POSTGRES HERE // : base off of whatever database handler we'll use in the end
+    {
+
+    }
+
+    [Index(nameof(UserName), IsUnique = true]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string ?Email { get; set; }
+        public string ?Phone { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+
+    public class CV
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CVID { get; set; }
+        public string Qualification { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+        public int AreaID { get; set; }
+        public Area Area { get; set; }
+    }
+
+    public class Area
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AreaID { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string County { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+
+    public class Education
+    {
+        [Required]
+        public string Institute { get; set; }
+        [Required]
+        public int Span { get; set; }
+        [Required]
+        public string Faculty { get; set; }
+        [Required]
+        public string Graduation { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+        public int AreaID { get; set; }
+        public Area Area { get; set; }
+    }
+
+    public class Company
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CompanyID { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        public string ?CompanyEmail { get; set; }
+        public string ?CompanyPhone { get; set; }
+        public int OwnerID { get { return User.UserID; } set { value = User.UserID; } }
+        public User User { get; set; }
+        public int AreaID { get; set; }
+        public Area Area { get; set; }
+    }
+
+    public class Branch
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BranchID { get; set; }
+        [Required]
+        public string BranchName { get; set; }
+        public string? BranchEmail { get; set; }
+        public string? BranchPhone { get; set; }
+        public int ManagerID { get { return User.UserID; } set { value = User.UserID; } }
+        public User User { get; set; }
+        public int AreaID { get; set; }
+        public Area Area { get; set; }
+        public int CompanyID { get; set; }
+        public Company Company { get; set; }
+    }
+
+    public class Job
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int JobID { get; set; }
+        public int Pay { get; set; }
+        public string WorkHours { get; set; }
+        public string Language { get; set; }
+        public int BranchID { get; set; }
+        public Branch Branch { get; set; }
+        public int AreaID { get; set; }
+        public Area Area { get; set; }
+    }
+
+    public class Rating
+    {
+        public int FRating { get; set; }
+        public string Feedback { get; set; }
+        public bool Anonymous { get; set; }
+        public int CompanyID { get; set; }
+        public Company Company { get; set; }
+        public int FeedbackUserID { get { return User.UserID; } set { value = User.UserID; } }
+        public User User { get; set; }
+    }
+
+    public class PreviuosEmployment
+    {
+        public string Provider { get; set; }
+        public string Description { get; set; }
+        public string Position { get; set; }
+        public int Stay { get; set; }
+        public DateTimeOffset Hired { get; set; }
+        public DateTimeOffset Resigned { get; set; }
+        public int ProviderID { get { return Company.CompanyID; } set { value = Company.CompanyID; } }
+        public Company Company { set; get; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+    }
+
+    public class Request
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.)]
+        public int RequestID { get; set; }
+        public string Status { get; set; }
+        public string Comment { get; set; }
+        public int JobID { get; set; }
+        public Job Job { set; get; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+        public int CVID { get; set; }
+        public CV CV { get; set; }
+        //public int CVIDID { get; set;
+        //public int CVIDIDID { get;set; }
+        //public int CVIDIDIDID { get; set; }
+    }
+}
--- API/JobHiringAPI/JobHiringAPI/Program.cs
+++ API/JobHiringAPI/JobHiringAPI/Program.cs
@@ -8,6 +8,8 @@ namespace JobHiringAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            
+            // builder.Services.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
