14a4565801ddb325231aef5ab9f54c8079e95c4c
local-lunatick <Legrady-Simon.Balint@C113-03>
2026-02-24 11:00:44 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Mobile/Avalonia_Admin_Interface/Views/MainWindow.axaml.cs
+++ Mobile/Avalonia_Admin_Interface/Views/MainWindow.axaml.cs
@@ -1,12 +1,13 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 
-namespace Avalonia_Admin_Interface.Views
+namespace Avalonia_Admin_Interface;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
