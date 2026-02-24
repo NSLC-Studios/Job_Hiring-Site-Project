2f779028649929c780a59d3a54174a84731f90da
local-lunatick <Legrady-Simon.Balint@C113-03>
2026-02-24 12:27:33 +0100

    fixed avalonia

---
+++ Mobile/AvaloniaAdminInterface/AvaloniaAdminInterface/ViewModels/RelayCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class RelayCommand
+{
+    private Action updateFunction;
+
+    public RelayCommand(Action execute)
+    {
+        updateFunction = execute;
+    }
+
+    public void Execute()
+    {
+        updateFunction?.Invoke();
+    }
+}
--- Mobile/AvaloniaAdminInterface/AvaloniaAdminInterface/ViewModels/User.cs
+++ Mobile/AvaloniaAdminInterface/AvaloniaAdminInterface/ViewModels/User.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Avalonia_Admin_Interface.Models
+namespace AvaloniaAdminInterface.ViewModels
 {
     public class User
     {
